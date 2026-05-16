8a0ad08fc3682805e3a5d0e3bdff829ea7b24a52
benit <polviedivine@gmail.com>
2026-05-16 19:51:15 +0200

    attepmt

--- 04-CRUD-IN-React/src/App.jsx
+++ 04-CRUD-IN-React/src/App.jsx
@@ -5,15 +5,32 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [todoList, steTodoList] = useState([])
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const task_id = todoList.length == 0? 1: todoList[todoList.length - 1].id + 1; 
+    setTodoList([...todoList, {id: task_id, task: newTask}]);
+  };
+  
+  const deleteTask = (id) => {
+    const tasks = todoList.filter((task) => task.id !== id)
+    setTodoList(tasks);
+  };
 
   return (
     <div className='App'>
       <div className='addTask'>
-        <input/>
-        <button>Add task</button>
+        <input onChange={handleChange}/>
+        <button onClick={addTask}>Add task</button>
       </div>
-      <div>"list"</div>
+      <div>{todoList.map((task) => {
+        return <div><h1>{task.task}</h1> <button onClick={()=> deleteTask(task.id)}>Delete</button></div>
+      })}</div>
     </div>
   )
 }
