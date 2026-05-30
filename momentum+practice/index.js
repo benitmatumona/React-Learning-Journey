5dd41a17c848fb207e91be3272c94e2062f444ad
benit <polviedivine@gmail.com>
2026-05-30 08:11:30 +0200

    practice

--- momentum+practice/index.js
+++ momentum+practice/index.js
@@ -1,3 +1,14 @@
-import crypto, { createHash } from "crypto"
+import fs from "fs"
+import crypto from "crypto"
 
-console.log(crypto.createHash("md5").update("test123").digest("hex"))
+const response = await fetch("https://jsonplaceholder.typicode.com/users/2")
+
+const data = await response.json()
+
+const {name, email} = data
+
+const data2 = JSON.stringify({name, email}, null, 2).toUpperCase()
+
+fs.writeFileSync("output.txt", data2)
+
+console.log(crypto.createHash("md5").update(fs.readFileSync("output.txt", "utf-8")).digest("hex"))
